ede1478a37a3d39c23ee8d86c0a1395547548b85
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-07-30 13:18:27 +0300

    Update fxmanifest.lua

--- tpz_characters/fxmanifest.lua
+++ tpz_characters/fxmanifest.lua
@@ -4,7 +4,7 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 
 author 'Nosmakos'
 description 'Titans Productions Zombies Core - Characters'
-version '1.0.0'
+version '1.0.1'
 
 ui_page 'html/index.html'
 
