64ad2246dc6ba119d57ca7fbf34092140acb3c58
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-12-14 12:58:48 +0200

    Update fxmanifest.lua

--- tpz_characters/fxmanifest.lua
+++ tpz_characters/fxmanifest.lua
@@ -4,7 +4,7 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 
 author 'Nosmakos'
 description 'TPZ-CORE - Characters'
-version '2.0.3'
+version '2.0.4'
 
 ui_page 'html/index.html'
 
