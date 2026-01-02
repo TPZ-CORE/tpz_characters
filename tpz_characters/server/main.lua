8cd612a7be5a6c4966d844babe060c15e5246dd5
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2026-01-02 21:57:34 +0200

    Update main.lua

--- tpz_characters/server/main.lua
+++ tpz_characters/server/main.lua
@@ -88,7 +88,7 @@ AddEventHandler("tpz_characters:requestCharacterSkin", function(targetSource)
             local charData = result[1]
             TriggerClientEvent('tpz_characters:receiveSkinData', _source, {
                 skin = charData.skin,
-                skinComp = charData.skinComp,
+                skinComp = skinComp,
                 gender = charData.gender
             })
         else
@@ -127,6 +127,7 @@ exports.tpz_core:getCoreAPI().addNewCallBack("tpz_characters:getPlayerSkinInform
         end
     end)
 end)
+
 
 
 
